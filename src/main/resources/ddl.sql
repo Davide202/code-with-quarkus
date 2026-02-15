474af15cbb518f7394d1f0c14564cb675b4e4d47
Davide D <davide.dinnocente@gmai.com>
2026-02-15 13:46:38 +0100

    fix

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -1,20 +1,23 @@
-DROP SCHEMA  if exists quarkus;
-create schema quarkus;
-CREATE TABLE `quarkus`.`tb_account_quarkus` (
-  `id` BIGINT NOT NULL,
-  `ACCOUNT_USERNAME` VARCHAR(45) NULL,
-  `ACCOUNT_PASSWORD` VARCHAR(45) NULL,
-  PRIMARY KEY (`id`));
+DROP SCHEMA IF EXISTS quarkus;
+CREATE SCHEMA quarkus;
+USE quarkus;
 
-  CREATE TABLE `quarkus`.`tb_user_quarkus` (
-    `id` BIGINT NOT NULL,
-    `USER_ACCOUNT` BIGINT NOT NULL,
-    `USER_NOME` VARCHAR(45) NULL,
-    `USER_COGNOME` VARCHAR(45) NULL,
-    PRIMARY KEY (`id`),
-    INDEX `user_account_idx` (`USER_ACCOUNT` ASC) VISIBLE,
-    CONSTRAINT `user_account`
-      FOREIGN KEY (`USER_ACCOUNT`)
-      REFERENCES `quarkus`.`tb_account_quarkus` (`id`)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION);
+CREATE TABLE tb_account_quarkus (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  ACCOUNT_USERNAME VARCHAR(40) UNIQUE,
+  ACCOUNT_PASSWORD VARCHAR(40),
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE tb_user_quarkus (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  USER_ACCOUNT BIGINT NOT NULL,
+  USER_NOME VARCHAR(40),
+  USER_COGNOME VARCHAR(40),
+  PRIMARY KEY (id),
+  CONSTRAINT user_account
+    FOREIGN KEY (USER_ACCOUNT)
+    REFERENCES tb_account_quarkus (id)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION
+);
